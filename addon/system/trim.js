47ae6017a5c384f17dd2e30ff556cca4f3ce5d22
Tim Evans <tim.evans@paddle8.com>
2014-11-18 10:23:28 -0500

    trim should handle null values

--- addon/system/trim.js
+++ addon/system/trim.js
@@ -2,13 +2,13 @@ var trim;
 
 if (String.prototype.trim) {
   trim = function (string) {
-    return string.trim();
+    return (string || '').trim();
   };
 } else {
   // Make sure we trim BOM and NBSP
   var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
   trim = function (string) {
-    return string.replace(rtrim, "");
+    return (string || '').replace(rtrim, "");
   };
 }
 
