93283a76b4f217b3413e9c64c693b0ff8a41651d
Tim Evans <tim-evans@sproutcore.com>
2015-03-05 19:51:07 -0500

    add config and init to MockUploader

--- tests/helpers/mock-uploader.js
+++ tests/helpers/mock-uploader.js
@@ -1,9 +1,15 @@
-export default function () {
+export default function (config) {
   return {
+    config: config,
+
     total: {
       queued: 0,
       size: 0,
       loaded: 0
+    },
+
+    init: function () {
+      this.initialized = true;
     },
 
     bind: function (functionName, callback) {
