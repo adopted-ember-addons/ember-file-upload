797fb3ee18cf521d7a7a67080f84f2f55f208f13
Tim Evans <tevans@movableink.com>
2015-08-12 11:13:45 -0400

    bump plupload to 2.1.8

--- blueprints/ember-plupload/index.js
+++ blueprints/ember-plupload/index.js
@@ -3,7 +3,7 @@ module.exports = {
 
   afterInstall: function() {
     var self = this;
-    return this.addBowerPackageToProject('plupload#2.1.3').then(function () {
+    return this.addBowerPackageToProject('plupload#2.1.8').then(function () {
       return self.addBowerPackageToProject('dinosheets#0.0.1');
     });
   }
