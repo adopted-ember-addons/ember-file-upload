2c8f892c6c475c699fd883bea6342d329a6ddf1a
Tim Evans <tevans@movableink.com>
2015-10-26 11:00:12 -0400

    handle both Ember 2.x and Ember 1.x for initializers

--- app/initializers/pl-uploader-manager.js
+++ app/initializers/pl-uploader-manager.js
@@ -4,10 +4,15 @@ import makeFileFilter from 'ember-plupload/system/make-file-filter';
 var keys = Object.keys;
 
 export function initialize(app) {
+  if (arguments[1]) { // Ember < 2.1
+    app = arguments[1];
+  }
+
   var entries = requirejs.entries;
   var fileFilterPrefix = app.modulePrefix + '/file-filters';
   var fileFilters = {};
   keys(entries).forEach(function (key) {
+
     if (key.indexOf(fileFilterPrefix) === 0) {
       var filterName = key.split('/').slice(-1);
       var module = require(key, null, null, true);
