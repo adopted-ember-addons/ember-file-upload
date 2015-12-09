384acf3e8cec512046be3eef3bdb7e6a06e717e7
Tim Evans <tevans@movableink.com>
2015-12-09 13:29:56 -0500

    update to dinosheets#0.1.1 with fixes for asynchrony with chrome

--- blueprints/ember-plupload/index.js
+++ blueprints/ember-plupload/index.js
@@ -6,7 +6,7 @@ module.exports = {
   afterInstall: function () {
     return RSVP.all([
       this.addBowerPackageToProject('plupload', 'v2.1.8'),
-      this.addBowerPackageToProject('dinosheets', '0.0.1'),
+      this.addBowerPackageToProject('dinosheets', '0.1.1'),
     ]);
   }
 };
