66a94694141b817bf618aa2ae818f9c9a5ecc26c
Tim Evans <tim.c.evans@me.com>
2016-10-21 00:11:01 -0400

    tests both uuid native and polyfill

--- tests/unit/system/uuid-test.js
+++ tests/unit/system/uuid-test.js
@@ -1,4 +1,4 @@
-import uuid from 'ember-file-upload/system/uuid';
+/* global require */
 import {
   module,
   test
@@ -6,12 +6,30 @@ import {
 
 module('uuid');
 
-test('long', function (assert) {
-  assert.equal(uuid().length, 36);
-  assert.notEqual(uuid(), uuid());
-});
+let unit = function (name) {
+  let module = {};
+  require.entries['ember-file-upload/system/uuid'].callback(module);
+  let uuid = module['default'];
 
-test('short', function (assert) {
-  assert.equal(uuid.short().length, 5);
-  assert.notEqual(uuid.short(), uuid.short());
-});
+  test(name + ' long', function (assert) {
+    assert.equal(uuid().length, 36);
+    assert.notEqual(uuid(), uuid());
+  });
+
+  test(name + ' short', function (assert) {
+    assert.equal(uuid.short().length, 5);
+    assert.notEqual(uuid.short(), uuid.short());
+  });
+};
+
+let crypto = window.crypto || window.msCrypto;
+if (crypto) {
+  unit('native');
+
+  let getRandomValues = window.crypto.getRandomValues;
+  crypto.getRandomValues = null;
+  unit('polyfill');
+  crypto.getRandomValues = getRandomValues;
+} else {
+  unit('polyfill');
+}
