db4f5959f208638d2723c12921874f82f10cff1d
Tim Evans <tim.c.evans@me.com>
2016-10-25 23:17:41 -0400

    start testing http-request

---
+++ tests/unit/system/http-request-test.js
@@ -0,0 +1,162 @@
+/* global FakeXMLHttpRequest */
+import HttpRequest from 'ember-file-upload/system/http-request';
+import {
+  module,
+  test,
+  skip
+} from 'qunit';
+
+module('http-request', {
+  beforeEach() {
+    this._XMLHttpRequest = window.XMLHttpRequest;
+    window.XMLHttpRequest = () => {
+      let request = new FakeXMLHttpRequest();
+      this.request = request;
+      this.respond = function (...args) {
+        request.respond(...args);
+      };
+      return request;
+    };
+    this.subject = new HttpRequest();
+  },
+  afterEach() {
+    this.subject = null;
+    window.XMLHttpRequest = this._XMLHttpRequest;
+  }
+});
+
+test('open with default options', function (assert) {
+  assert.equal(this.request.readyState, FakeXMLHttpRequest.UNSENT);
+  this.subject.open('POST', 'http://emberjs.com');
+
+  assert.equal(this.request.readyState, FakeXMLHttpRequest.OPENED);
+  assert.equal(this.request.method, 'POST');
+  assert.equal(this.request.url, 'http://emberjs.com');
+});
+
+test('open ignores the async option', function (assert) {
+  this.subject.open('POST', 'http://emberjs.com', false);
+  assert.equal(this.request.async, true);
+});
+
+test('open allows username / password', function (assert) {
+  this.subject.open('POST', 'http://emberjs.com', true, 'tomster', 'ember');
+  assert.equal(this.request.username, 'tomster');
+  assert.equal(this.request.password, 'ember');
+});
+
+test('setRequestHeader', function (assert) {
+  this.subject.open();
+  this.subject.setRequestHeader('Content-Type', 'application/json');
+
+  assert.equal(this.request.requestHeaders['Content-Type'], 'application/json');
+});
+
+test('successful send with a text/plain response', function (assert) {
+  this.subject.open('PUT', 'http://emberjs.com');
+  let promise = this.subject.send({
+    filename: 'rfc.md'
+  }).then(function (response) {
+    assert.deepEqual(response, {
+      body: 'ok',
+      headers: {
+        'Content-Type': 'text/plain'
+      },
+      status: 200
+    });
+  });
+
+  assert.deepEqual(this.request.requestBody, {
+    filename: 'rfc.md'
+  });
+
+  this.respond(200, { 'Content-Type': 'text/plain' }, 'ok');
+
+  return promise;
+});
+
+
+test('successful send with a text/html response', function (assert) {
+  this.subject.open('PUT', 'http://emberjs.com');
+  let promise = this.subject.send({
+    filename: 'rfc.md'
+  }).then(function (response) {
+    assert.deepEqual(response.headers, {
+      'Content-Type': 'text/html'
+    });
+    assert.equal(response.body[0].textContent, 'ok');
+  });
+
+  assert.deepEqual(this.request.requestBody, {
+    filename: 'rfc.md'
+  });
+
+  this.respond(200, { 'Content-Type': 'text/html' }, '<html><body>ok</body></html>');
+
+  return promise;
+});
+
+test('successful send with a text/xml response', function (assert) {
+  let xml = '<message from="zoey@emberjs.com" to="tomster@emberjs.com"><file name="rfc.md" size="1024"/></message>';
+
+  this.subject.open('PUT', 'http://emberjs.com');
+  let promise = this.subject.send({
+    filename: 'rfc.md'
+  }).then(function (response) {
+    assert.deepEqual(response.headers, {
+      'Content-Type': 'text/xml'
+    });
+    assert.equal(response.body.firstChild.outerHTML, xml);
+  });
+
+  assert.deepEqual(this.request.requestBody, {
+    filename: 'rfc.md'
+  });
+
+  this.respond(200, { 'Content-Type': 'text/xml' }, xml);
+
+  return promise;
+});
+
+['application/json', 'text/javascript', 'application/javascript'].forEach(function (contentType) {
+  test(`successful send with a ${contentType} response`, function (assert) {
+    this.subject.open('PUT', 'http://emberjs.com');
+    let promise = this.subject.send({
+      filename: 'rfc.md'
+    }).then(function (response) {
+      assert.deepEqual(response, {
+        body: {
+          name: 'rfc.md',
+          size: 1024
+        },
+        headers: {
+          'Content-Type': contentType
+        },
+        status: 200
+      });
+    });
+
+    assert.deepEqual(this.request.requestBody, {
+      filename: 'rfc.md'
+    });
+
+    this.respond(200, { 'Content-Type': contentType }, JSON.stringify({
+      name: 'rfc.md',
+      size: 1024
+    }));
+
+    return promise;
+  });
+});
+
+skip('onprogress', function () {
+
+});
+
+skip('ontimeout', function () {
+
+});
+
+skip('onabort', function () {
+
+});
