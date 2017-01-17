39b3ed6889accbc1080dc702a686aa53e6bd0bfb
Tim Evans <tim.c.evans@me.com>
2017-01-16 22:17:19 -0500

    support other svg elements

--- addon/components/file-upload/component.js
+++ addon/components/file-upload/component.js
@@ -9,7 +9,8 @@ const VALID_TAGS = ['a', 'abbr', 'area', 'audio', 'b', 'bdo', 'br', 'canvas', 'c
                     'code', 'command', 'datalist', 'del', 'dfn', 'em', 'embed', 'i',
                     'iframe', 'img', 'kbd', 'mark', 'math', 'noscript', 'object', 'q',
                     'ruby', 'samp', 'script', 'small', 'span', 'strong', 'sub', 'sup',
-                    'svg', 'time', 'var', 'video', 'wbr'];
+                    'svg', 'time', 'var', 'video', 'wbr',
+                    'path', 'g', 'use'];
 
 export default Ember.Component.extend({
   tagName: 'label',
