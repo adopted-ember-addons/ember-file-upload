a81251e9bdc09e1d7a0f6ed77e6af1229dcc3800
Tim Evans <tim_evans@condenast.com>
2017-03-15 14:03:53 -0400

    theme the demo

--- tests/dummy/app/index/route.js
+++ tests/dummy/app/index/route.js
@@ -1,17 +1,33 @@
 import Ember from 'ember';
 
+const { get, setProperties } = Ember;
+
 export default Ember.Route.extend({
   model() {
-    return Ember.A();
+    return Ember.A([{
+      filename: 'little-green-men.jpg',
+      preview: '/assets/images/little-green-men.jpg',
+      type: 'image'
+    }, {
+      filename: 'samantha-mulder.jpg',
+      preview: '/assets/images/samantha-mulder.jpg',
+      type: 'image'
+    }]);
   },
 
   actions: {
-    uploadImage: function (file) {
+    uploadProof (file) {
+      let asset = {
+        filename: get(file, 'name'),
+        file
+      };
+      this.currentModel.pushObject(asset);
       return file.upload('/photos/new').then(({ body: { filename, url, type } }) => {
-        this.currentModel.pushObject({
+        setProperties(asset, {
           filename,
           preview: url,
-          type
+          type,
+          file: null
         });
       }, function () {});
     }
