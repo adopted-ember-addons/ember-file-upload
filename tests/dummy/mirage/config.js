0aeb6ac14ed5ac9647b82f6e5c1a422b6e087791
Tim Evans <tim_evans@condenast.com>
2017-06-29 15:26:28 -0400

    use rootURL for configuring the route to fetch data.json

--- tests/dummy/mirage/config.js
+++ tests/dummy/mirage/config.js
@@ -1,8 +1,9 @@
 import { upload } from 'ember-file-upload/mirage';
+import config from '../config/environment';
 
 export default function () {
   this.passthrough('/write-coverage');
-  this.passthrough('/docs/data.json');
+  this.passthrough(`${config.rootURL}docs/data.json`);
   this.post('/photos/new', upload(function (db, request) {
     let { type, name, size, url } = request.requestBody.file;
     return {
