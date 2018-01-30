fcccb0510a9e7d919a957ebb7ebad7b4cb26d8cb
Tim Evans <tim_evans@condenast.com>
2018-01-30 14:34:32 -0500

    update to Ember 2.18

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -1,19 +1,12 @@
-import Ember from 'ember';
+import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
-const App = Ember.Application.extend({
+const App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver,
-  engines: {
-    addonDocs: {
-      dependencies: {
-        services: ['config']
-      }
-    }
-  }
+  Resolver
 });
 
 loadInitializers(App, config.modulePrefix);
