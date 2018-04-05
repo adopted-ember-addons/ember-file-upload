4a22a78c58e733c8c6f5254a2090eff400a0748b
Raido Kuli <raido357@gmail.com>
2018-04-05 21:27:20 +0300

    Demo is back

---
+++ tests/dummy/app/routes/index.js
@@ -0,0 +1,16 @@
+import { A } from '@ember/array';
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+  model() {
+    return A([{
+      filename: 'little-green-men.jpg',
+      preview: '/assets/images/little-green-men.jpg',
+      type: 'image'
+    }, {
+      filename: 'samantha-mulder.jpg',
+      preview: '/assets/images/samantha-mulder.jpg',
+      type: 'image'
+    }]);
+  }
+});
