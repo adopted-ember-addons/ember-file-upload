0708fc3c1aa5a916b4167b542df3abe3d8e45798
Tim Evans <tim.c.evans@me.com>
2018-04-08 18:15:36 -0400

    :no_good: remove fingerprinting for docs
    I believe that this was causing the paths to be generated incorrectly for the docs

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,10 +5,6 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
     // Add options here
-    fingerprint: {
-      extensions: ['js', 'css', 'png', 'jpg', 'gif', 'map', 'eot', 'ttf', 'woff', 'woff2', 'svg'],
-      prepend: '/ember-file-upload/'
-    },
     sassOptions: {
       includePaths: ['tests/dummy/app']
     },
