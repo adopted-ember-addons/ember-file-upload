13dd227a2546a468ac3ec4f0481c69aa57d29c4d
Tim Evans <tim.c.evans@me.com>
2018-11-04 21:07:36 -0500

    passthrough more mirage routes

--- tests/dummy/mirage/config.js
+++ tests/dummy/mirage/config.js
@@ -2,8 +2,8 @@ import { upload } from 'ember-file-upload/mirage';
 import config from '../config/environment';
 
 export default function () {
-  this.passthrough('/write-coverage');
-  this.passthrough('/versions.json');
+  this.passthrough(`${config.rootURL}write-coverage`);
+  this.passthrough(`${config.rootURL}versions.json`);
   this.passthrough(`${config.rootURL}docs/ember-file-upload.json`);
   this.passthrough(`${config.rootURL}ember-cli-addon-docs/search-index.json`);
   this.post('/photos/new', upload(function (db, request) {
