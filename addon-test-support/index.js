84533fd2d24f831cc57996747903adb5d1e46902
Andrey Fel <andrey.fel@retailnext.net>
2019-05-09 22:23:30 +0300

    Update ember-qunit (#237)
    Update ember-qunit to latest. It also bumps ember test helpers to latest.
    Update test helper 'upload' to be compatible with the latest ember
    test-helpers.

--- addon-test-support/index.js
+++ addon-test-support/index.js
@@ -6,15 +6,5 @@ export async function upload(selector, file) {
   assert(`Selector '${selector}' is not input element.`, input.tagName === 'INPUT');
   assert(`File must be instance of File/Blob type`, file instanceof Blob);
 
-  // This hack is here since we can't mock out the
-  // FileList API easily; we're taking advantage
-  // that we can mutate the FileList DOM API at
-  // runtime to allow us to push files into the <input>
-  let files = [file];
-  input.files.item = function (idx) {
-    return files[idx];
-  };
-  input.files.size = files.length;
-
-  return await triggerEvent(input, 'change');
+  return await triggerEvent(input, 'change', { files: [file] });
 }
