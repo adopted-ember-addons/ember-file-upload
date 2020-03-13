e543fdc5c4aec3934971a36fa39cbceccfc3f73a
Nathanael Dekeister <nathanael_dekeister@ultimatesoftware.com>
2020-03-13 18:03:22 +0100

    build: add edge to dummy app targets

--- tests/dummy/config/targets.js
+++ tests/dummy/config/targets.js
@@ -3,7 +3,8 @@
 const browsers = [
   'last 1 Chrome versions',
   'last 1 Firefox versions',
-  'last 1 Safari versions'
+  'last 1 Safari versions',
+  'last 1 edge versions'
 ];
 
 module.exports = {
