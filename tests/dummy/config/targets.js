d7e48ddd0b5688dfea9f358d3cd4c769f7381a8b
Jeldrik Hanschke <admin@jhanschke.de>
2020-04-14 09:00:58 +0200

    build targets should cover a wider browser range
    This also fixes the syntax error in common Edge versions.

--- tests/dummy/config/targets.js
+++ tests/dummy/config/targets.js
@@ -1,10 +1,14 @@
 'use strict';
 
 const browsers = [
-  'last 1 Chrome versions',
-  'last 1 Firefox versions',
-  'last 1 Safari versions',
-  'last 1 edge versions'
+  'last 2 Chrome versions',
+  'last 2 Firefox versions',
+  'Firefox ESR',
+  'last 2 Safari versions',
+  // Last versions of Microsoft Edge are build on top of Chromium. But they are
+  // not shipped yet to a significant number of users. Need to still support
+  // Edge 18 explicitly until chromium-based Edge is shipped to all users.
+  'Edge >= 18'
 ];
 
 module.exports = {
