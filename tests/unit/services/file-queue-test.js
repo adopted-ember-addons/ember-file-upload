89daad5e7762e2aa34ffd0cd1702dabc8f3dc3ae
Giles Thompson <giles@papaiti.com>
2021-09-01 17:42:33 +1200

    tidy(file queue test): remove run usage - hopefully it's not necessary

--- tests/unit/services/file-queue-test.js
+++ tests/unit/services/file-queue-test.js
@@ -1,4 +1,3 @@
-import { run } from '@ember/runloop';
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import File from 'ember-file-upload/file';
@@ -8,9 +7,9 @@ module('service:file-queue', function (hooks) {
 
   test('the size of the queue is the aggregate of all queues', function (assert) {
     var queue = this.owner.lookup('service:file-queue');
-    var queue1 = run(queue, 'create', 'queue1');
-    var queue2 = run(queue, 'create', 'queue2');
-    run(queue, 'create', 'queue3');
+    var queue1 = queue.create('queue1');
+    var queue2 = queue.create('queue2');
+    queue.create('queue3');
 
     assert.equal(queue.files.length, 0);
     assert.equal(queue.size, 0);
@@ -56,7 +55,7 @@ module('service:file-queue', function (hooks) {
 
   test('the uploaded size of the queue is the aggregate of all queues', function (assert) {
     var queue = this.owner.lookup('service:file-queue');
-    var queue1 = run(queue, 'create', 'queue1');
+    var queue1 = queue.create('queue1');
 
     assert.equal(queue.files.length, 0);
     assert.equal(queue.size, 0);
@@ -75,7 +74,7 @@ module('service:file-queue', function (hooks) {
     assert.equal(queue.loaded, 500);
     assert.equal(queue.progress, 25);
 
-    var queue2 = run(queue, 'create', 'queue2');
+    var queue2 = queue.create('queue2');
 
     queue2.push({
       id: 'test1',
@@ -89,7 +88,7 @@ module('service:file-queue', function (hooks) {
     assert.equal(queue.loaded, 1000);
     assert.equal(queue.progress, 18);
 
-    run(queue, 'create', 'queue3');
+    queue.create('queue3');
 
     queue2.push({
       id: 'test2',
@@ -106,7 +105,7 @@ module('service:file-queue', function (hooks) {
 
   test('the queue is emptied when all files are completed', function (assert) {
     var queue = this.owner.lookup('service:file-queue');
-    var queue1 = run(queue, 'create', 'queue1');
+    var queue1 = queue.create('queue1');
 
     const file0 = new File();
     file0.state = 'queued';
