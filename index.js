1e32b0c23ebaa2d17b3c1cf558cafeab206fab73
Giles Thompson <giles@papaiti.com>
2021-10-15 12:13:22 +1300

    fix(embroider builds): conditionally exclude mirage handler unless ember-cli-mirage is enabled Fix CSP for embroider builds by switching the source mapping method to not use eval()

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 'use strict';
+const funnel = require('broccoli-funnel');
 
 module.exports = {
   name: require('./package').name,
+  included(app) {
+    let config = app.project.config();
+
+    this.appEnv = config.environment;
+    this.mirageConfig = config['ember-cli-mirage'] || {};
+
+    this._super.included.apply(this, arguments);
+  },
+  treeForAddon() {
+    let tree = this._super.treeForAddon.apply(this, arguments);
+    return this._shouldIncludeMirageHandler()
+      ? tree
+      : funnel(tree, { exclude: ['ember-file-upload/mirage/*'] });
+  },
+  _shouldIncludeMirageHandler() {
+    if (process.env.EMBER_CLI_FASTBOOT) return false;
+
+    let environment = this.appEnv;
+    let enabledInProd =
+      environment === 'production' && this.mirageConfig.enabled;
+    let explicitExcludeFiles = this.mirageConfig.excludeFilesFromBuild;
+    let shouldIncludeHandler =
+      enabledInProd ||
+      (environment &&
+        environment !== 'production' &&
+        explicitExcludeFiles !== true);
+
+    return shouldIncludeHandler;
+  },
 };
