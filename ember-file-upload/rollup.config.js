401c981bbb87fa6178c4db6234ea1df91d0d1d5f
Giles Thompson <giles@papaiti.com>
2022-03-31 12:31:50 +1300

    feat(v2 addon): configure rollup using rollup-plugin-ts

--- ember-file-upload/rollup.config.js
+++ ember-file-upload/rollup.config.js
@@ -1,4 +1,6 @@
-import babel from '@rollup/plugin-babel';
+import ts from 'rollup-plugin-ts';
+import { defineConfig } from 'rollup';
+
 import { Addon } from '@embroider/addon-dev/rollup';
 
 const addon = new Addon({
@@ -6,35 +8,42 @@ const addon = new Addon({
   destDir: 'dist',
 });
 
-export default {
-  // This provides defaults that work well alongside `publicEntrypoints` below.
-  // You can augment this if you need to.
+export default defineConfig({
   output: addon.output(),
-
   plugins: [
     // These are the modules that users should be able to import from your
     // addon. Anything not listed here may get optimized away.
-    addon.publicEntrypoints([
-      'components/**/*.js',
-      'helpers/**/*.js',
-      'services/**/*.js',
-      'system/**/*.js',
-    ]),
+    addon.publicEntrypoints(['**/*.{js,ts}']),
 
     // These are the modules that should get reexported into the traditional
     // "app" tree. Things in here should also be in publicEntrypoints above, but
     // not everything in publicEntrypoints necessarily needs to go here.
     addon.appReexports([
-      'components/**/*.js',
-      'helpers/**/*.js',
-      'services/**/*.js',
+      'components/**/*.{js,ts}',
+      'helpers/**/*.{js,ts}',
+      'modifiers/**/*.{js,ts}',
+      'services/**/*.{js,ts}',
     ]),
-
     // This babel config should *not* apply presets or compile away ES modules.
     // It exists only to provide development niceties for you, like automatic
     // template colocation.
     // See `babel.config.json` for the actual Babel configuration!
-    babel({ babelHelpers: 'bundled' }),
+    ts({
+      // can be changed to swc or other transpilers later
+      // but we need the ember plugins converted first
+      // (template compilation and co-location)
+      transpiler: 'babel',
+      browserslist: ['last 2 firefox versions', 'last 2 chrome versions'],
+      tsconfig: {
+        fileName: 'tsconfig.json',
+        hook: (config) => ({ ...config, declaration: true }),
+      },
+    }),
+
+    // Follow the V2 Addon rules about dependencies. Your code can import from
+    // `dependencies` and `peerDependencies` as well as standard Ember-provided
+    // package names.
+    addon.dependencies(),
 
     // Ensure that standalone .hbs files are properly integrated as Javascript.
     addon.hbs(),
@@ -43,7 +52,6 @@ export default {
     // to leave alone and keep in the published output.
     addon.keepAssets(['**/*.css']),
 
-    // Remove leftover build artifacts when starting a new build.
     addon.clean(),
   ],
-};
+});
