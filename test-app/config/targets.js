100ec283ecd68fbe047ef6ae04a892e01ed78920
Giles Thompson <giles@papaiti.com>
2023-06-10 13:42:49 +1200

    chore(test-app): update browser targets from latest ember-cli blueprint

--- test-app/config/targets.js
+++ test-app/config/targets.js
@@ -1,14 +1,9 @@
 'use strict';
 
 const browsers = [
-  'last 2 Chrome versions',
-  'last 2 Firefox versions',
-  'Firefox ESR',
-  'last 2 Safari versions',
-  // Last versions of Microsoft Edge are build on top of Chromium. But they are
-  // not shipped yet to a significant number of users. Need to still support
-  // Edge 18 explicitly until chromium-based Edge is shipped to all users.
-  'Edge >= 18',
+  'last 1 Chrome versions',
+  'last 1 Firefox versions',
+  'last 1 Safari versions',
 ];
 
 module.exports = {
