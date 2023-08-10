0d43973973441b2853479c01f5a09e4dbd19fc17
Giles Thompson <giles@papaiti.com>
2023-08-10 16:47:03 +1200

    Cast queue name to string when rendering it in test-app (#971)
    Since this type can be a symbol, Glint does not consider it to be a good value to render.

--- test-app/types/global.d.ts
+++ test-app/types/global.d.ts
@@ -1,6 +1,7 @@
 import '@glint/environment-ember-loose';
 import type EmberFileUploadRegistry from 'ember-file-upload/template-registry';
 import DemoUpload from 'test-app/components/demo-upload';
+import toString from 'test-app/helpers/to-string';
 
 // Types for compiled templates
 declare module 'test-app/templates/*' {
@@ -20,5 +21,6 @@ declare module '@glint/environment-ember-loose/registry' {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export default interface Registry extends EmberFileUploadRegistry {
     DemoUpload: typeof DemoUpload;
+    'to-string': typeof toString;
   }
 }
