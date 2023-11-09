6750c7406e44056566a8de8625796783c363bf27
Giles Thompson <giles@papaiti.com>
2023-11-10 12:40:13 +1300

    refactor(system upload): isolate onloadstart, onprogress, and onloadend for unit testing purposes

--- ember-file-upload/src/internal.ts
+++ ember-file-upload/src/internal.ts
@@ -1,10 +1,14 @@
 import DataTransferWrapper from './system/data-transfer-wrapper.ts';
 import HTTPRequest from './system/http-request.ts';
 import UploadFileReader from './system/upload-file-reader.ts';
+import { onloadstart, onprogress, onloadend } from './system/upload.ts';
 
 export {
   // Non-public classes imported by the test app
   DataTransferWrapper,
   HTTPRequest,
   UploadFileReader,
+  onloadstart,
+  onprogress,
+  onloadend,
 };
