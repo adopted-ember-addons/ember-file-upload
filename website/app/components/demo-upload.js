4c0887059f45a6fc95d3121011383da3a61e283c
Robbie Wagner <rwwagner90@gmail.com>
2023-11-13 07:12:26 -0800

    Add estimated time to demo

--- website/app/components/demo-upload.js
+++ website/app/components/demo-upload.js
@@ -29,6 +29,27 @@ export default class DemoUploadComponent extends Component {
     this.simulateUpload.perform(file);
   }
 
+  estimatedTimeRemaining = (loaded, rate, size) => {
+    const bytesLoaded = loaded ?? 0;
+    const fileSize = size ?? 0;
+
+    if (bytesLoaded) {
+      const seconds = (fileSize - bytesLoaded) / (rate || 1);
+
+      const date = new Date(seconds * 1000);
+      const days = Math.floor(seconds / 86400);
+
+      const daysRem = days > 0 ? `${days}d ` : '';
+      const hoursRem = String(date.getUTCHours()).padStart(2, '0');
+      const minutesRem = String(date.getUTCMinutes()).padStart(2, '0');
+      const secondsRem = String(date.getUTCSeconds()).padStart(2, '0');
+
+      return `${daysRem}${hoursRem}:${minutesRem}:${secondsRem}`;
+    }
+
+    return '?';
+  };
+
   @task({ enqueue: true })
   *simulateUpload(file) {
     file.state = FileState.Uploading;
