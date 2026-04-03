cfdcef16a54b6b690c9b90878c655db68ecd3d46
Robert Wagner <rwwagner90@gmail.com>
2026-04-03 09:10:05 -0400

    Update try scenarios

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -8,14 +8,26 @@ module.exports = async function () {
     usePnpm: true,
     scenarios: [
       {
-        name: 'ember-lts-3.28',
+        name: 'ember-lts-4.4',
         npm: {
           devDependencies: {
-            'ember-cli': '~4.12.2',
-            'ember-source': '~3.28.12',
-            '@ember/test-helpers': '^2.9.4',
-            'ember-qunit': '~6.0.0',
-            '@glimmer/component': '^1.1.2'
+            'ember-source': '~4.4.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.12',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.12.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-5.4',
+        npm: {
+          devDependencies: {
+            'ember-source': '~5.4.0',
           },
         },
       },
